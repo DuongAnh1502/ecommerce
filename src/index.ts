39e37cfe97ca5466219b068a19d719839f71615c
duonganh203 <rankill52@gmail.com>
2024-02-03 16:02:59 +0700

    add root router

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
+import rootRouter from "./routes";
 interface ResType {
     req: Request;
     res: Response;
 }
 const app: Express = express();
 
-app.get("/", ({ req, res }: ResType) => {
-    res.send("Hello World");
-});
-
 app.listen(PORT, () => {
     console.log("Server is running on port 5555");
     console.log("http://localhost:5555");
 });
+
+app.use("/api", rootRouter);
